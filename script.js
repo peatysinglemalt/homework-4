f7bbd2b1323150d318f9bf3d138c5390ae83180c
mateo's mbp <mateodubs@mateos-MacBook-Pro.local>
2020-12-22 07:44:51 -0800

    udpated js

---
+++ script.js
@@ -0,0 +1,26 @@
+var questions = [
+    {   title: "When did the Cold War end?",
+        choices: ["1973", "1989", "1992", "1985"],
+        answer: "1989"
+    },
+    {   title: "What color is Absynthe?", 
+        choices: ["yellow", "green", "blue", "black"],
+        answer: "green"
+    },
+    {   title: "What state is the Lincoln family home (Hildene) located in?",
+        choices: ["Vermont", "Maine", "Pennsylvania", "Virginia"],  
+        answer: "Vermont"
+    },
+    {   title: "What is the diameter of Earth?",
+        choices: ["10,000 miles", "23,000 miles", "8,000 miles", "16,000 miles"],
+        answer: "8,000 miles"
+    },
+    {   title: "Kodiak island is in which US state?",
+        choices: ["Hawaii", "Florida", "New York", "Alaska"],
+        answer: "Alaska"
+    },
+    {   title: "Which reality show series is Andy Cohen’s favorite?",
+    choices: ["Survivor", "The Bachelorette", "The Real Housewives", "Top Chef"],
+    answer: "The Real Housewives"
+}
+];
