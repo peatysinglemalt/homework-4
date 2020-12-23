da79d3235c4115ea2b1c7fc093869d9b7b4b277a
mateo's mbp <mateodubs@mateos-MacBook-Pro.local>
2020-12-22 19:21:48 -0800

    commit

--- README.md
+++ README.md
@@ -1,3 +1,54 @@
+## What this is
+Web APIs: Code Quiz, aka, Triva Game
+
+## Technology Used
+JavaScript, HTML, CSS
+
+## What you did
+
+- created a trivia game with 6 unique questions 
+- once the begin trivia button is clicked, the game begins and timer starts
+- once a question is clicked, it moves to the next question 
+- if the question is answered incorrectly, the timer is reduced by 10 seconds 
+- when all the questions are answered, user is alerted of the number of questions answered correctly 
+- user is presented with an input box to add initials and log their name 
+- user is able to view high score on the nav bar 
+
+
+## Code Snippets With context as to why you are highlighting it (Do not highlight every line of code)
+Snippet of function start, and timer code 
+```
+function start() {
+    timeLeft = 60;
+    document.getElementById("timeLeft").innerHTML = timeLeft;
+
+    timer = setInterval(function () {
+        timeLeft--;
+        document.getElementById("timeLeft").innerHTML = timeLeft;
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+            endGame();
+        }
+    }, 1000);
+
+    next();
+}
+```
+
+## Who You Are (links to your LinkedIn & Portfolio - if one exists)
+[LinkedIn](https://www.linkedin.com/in/matthewywu/)
+
+## Website
+
+[GitHub repository](https://peatysinglemalt.github.io/homework-3/)
+[Deployed Site] 
+
+## Screenshots
+
+![screenshot 1]
+![screenshot 2]
+![screenshot 3]
+
 # 04 Web APIs: Code Quiz
 
 As you proceed in your career as a web developer, you will probably be asked to complete a coding assessment, which is typically a combination of multiple-choice questions and interactive challenges. Build a timed code quiz with multiple-choice questions. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
@@ -25,18 +76,3 @@ THEN the game is over
 WHEN the game is over
 THEN I can save my initials and score
 ```
-
-The following animation demonstrates the application functionality:
-
-![code quiz](./Assets/04-web-apis-homework-demo.gif)
-
-### Review
-
-You are required to submit the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
-- - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
--- script.js
+++ script.js
@@ -1,26 +1,140 @@
-var questions = [
-    {   title: "When did the Cold War end?",
-        choices: ["1973", "1989", "1992", "1985"],
+var trivia = [
+    {   triviaQuestion: "When did the Cold War end?",
+        choice: ["1973", "1989", "1992", "1985"],
         answer: "1989"
     },
-    {   title: "What color is Absynthe?", 
-        choices: ["yellow", "green", "blue", "black"],
+    {   triviaQuestion: "What color is Absynthe?", 
+        choice: ["yellow", "green", "blue", "black"],
         answer: "green"
     },
-    {   title: "What state is the Lincoln family home (Hildene) located in?",
-        choices: ["Vermont", "Maine", "Pennsylvania", "Virginia"],  
+    {   triviaQuestion: "What state is the Lincoln family home (Hildene) located in?",
+        choice: ["Vermont", "Maine", "Pennsylvania", "Virginia"],  
         answer: "Vermont"
     },
-    {   title: "What is the diameter of Earth?",
-        choices: ["10,000 miles", "23,000 miles", "8,000 miles", "16,000 miles"],
+    {   triviaQuestion: "What is the diameter of Earth?",
+        choice: ["10,000 miles", "23,000 miles", "8,000 miles", "16,000 miles"],
         answer: "8,000 miles"
     },
-    {   title: "Kodiak island is in which US state?",
-        choices: ["Hawaii", "Florida", "New York", "Alaska"],
+    {   triviaQuestion: "Kodiak island is in which US state?",
+        choice: ["Hawaii", "Florida", "New York", "Alaska"],
         answer: "Alaska"
     },
-    {   title: "Which reality show series is Andy Cohen’s favorite?",
-    choices: ["Survivor", "The Bachelorette", "The Real Housewives", "Top Chef"],
-    answer: "The Real Housewives"
-}
+    {   triviaQuestion: "Which reality show series is Andy Cohen’s favorite?",
+        choice: ["Survivor", "The Bachelorette", "The Real Housewives", "Top Chef"],
+        answer: "The Real Housewives"
+    }
 ];
+
+var score = 0;
+var currentQuestion = -1;
+var timeLeft = 0;
+var timer;
+
+function start() {
+    timeLeft = 60;
+    document.getElementById("timeLeft").innerHTML = timeLeft;
+
+    timer = setInterval(function () {
+        timeLeft--;
+        document.getElementById("timeLeft").innerHTML = timeLeft;
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+            endGame();
+        }
+    }, 1000);
+
+    next();
+}
+
+function next() {
+    currentQuestion++;
+
+    if (currentQuestion > trivia.length - 1) {
+        endGame();
+        return;
+    }
+
+    var quizContent = "<h2>" + trivia[currentQuestion].triviaQuestion + "</h2>"
+
+    for (var buttonLoop = 0; buttonLoop < trivia[currentQuestion].choice.length; buttonLoop++) {
+        var buttonCode = "<button class='answerButton' onclick=\"[ANS]\">[CHOICE]</button>";
+        buttonCode = buttonCode.replace("[CHOICE]", trivia[currentQuestion].choice[buttonLoop]);
+        if (trivia[currentQuestion].choice[buttonLoop] == trivia[currentQuestion].answer) {
+            buttonCode = buttonCode.replace("[ANS]", "correct()");
+        } else {
+            buttonCode = buttonCode.replace("[ANS]", "incorrect()");
+        }
+        quizContent += buttonCode
+    }
+
+    document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+function endGame() {
+    clearInterval(timer);
+
+    var quizContent = `
+    <h2>Well Done!</h2>
+    <h3>You answered ` + score + ` / 6 correctly!</h3>
+    <input type="text" id="name" placeholder="Initials"> 
+    <button onclick="setScore()">Submit</button>`;
+
+    document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+function setScore() {
+   
+    highscores.push(score);
+    highscores.sort(function (a, b) {
+      return b - a;
+    });
+   
+    console.log(highscores);
+    localStorage.setItem("highscores", JSON.stringify(highscores));
+    console.log("local storage:" + localStorage.highscores[1]);
+    localStorage.setItem("highscoreName", document.getElementById("name").value);
+    getScore();
+  }
+  
+function getScore() {
+  let finalHighScore = localStorage.highscores[1];
+  var quizContent =
+    `<h2>` + localStorage.getItem("highscoreName") + `'s current highscore:</h2>
+    <h1>` + finalHighScore + `</h1><br> 
+    <button onclick="resetGame()">Go Back</button><button onclick="clearScore()">Clear Highscore</button>
+    `;
+
+  document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+function clearScore() {
+    localStorage.setItem("highscores", "");
+    localStorage.setItem("highscoreName", "");
+
+    resetGame();
+}
+
+function incorrect() {
+    timeLeft -= 10;
+    next();
+}
+
+function correct() {
+    score += 1;
+    next();
+}
+
+function resetGame() {
+    clearInterval(timer);
+    score = 0;
+    currentQuestion = -1;
+    timeLeft = 0;
+    timer = null;
+
+    document.getElementById("timeLeft").innerHTML = timeLeft;
+
+    var quizContent = "";
+    document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+let highscores = JSON.parse(localStorage.getItem("highscores")) || [];
